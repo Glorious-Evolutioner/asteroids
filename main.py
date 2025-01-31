abfcc79866583b9036706c06df9dca36c8e266c7
Glorious-Evolutioner <kamilla.kaydarovaa@gmail.com>
2025-01-31 22:02:22 +0500

    Done with Player chapter moving to asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -9,14 +11,23 @@ def main():
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatables = pygame.sprite.Group(player) 
+    drawables = pygame.sprite.Group(player)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables)
+    some_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         time = clock.tick(60)
         dt = time/1000
-        screen.fill((0,0,0))
-        player.draw(screen)
+        updatables.update(dt)
+        screen.fill("black")
+        for thing in drawables:
+            thing.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
